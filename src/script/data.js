9b0627bd2913cad9353c6fdb24f96b1e9347d18f
ShadowMoose <theshadowmoose@gmail.com>
2017-12-27 22:25:20 -0700

    Fully working as far as I know, needs massive cleanup.

--- src/script/data.js
+++ src/script/data.js
@@ -7,7 +7,7 @@ class Gem{
 
 
 	/** Returns an array of formatted text lines, showing this aura's effects at the given level. */
-	level_stats(level){
+	level_stats(level, increase){
 		let lvl = level;//Math.max(1, Math.min(this.level_array.length, level));
 		console.log(this.name, '  level: ', lvl);
 		let stats = this.level_array[lvl]['stats'];
@@ -25,7 +25,7 @@ class Gem{
 		let groups = Gem.group_stats(ret);
 		//console.log(groups);
 
-		let final = Gem.convert_to_pob(groups);
+		let final = Gem.convert_to_pob(groups, increase);
 		console.log(final);
 		return final;
 	}
@@ -127,16 +127,17 @@ class Gem{
 
 
 	/** Convert this Aura Gem into a Path of Building-friendly string. */
-	static convert_to_pob(grouped_stats){
+	static convert_to_pob(grouped_stats, increase){
 		let final = [];
 		Object.keys(grouped_stats).forEach((key)=> {
 			let val_array = grouped_stats[key];
 			let out = this.convert_internal_name(key);
 			if(!out)return;
 			val_array.forEach((val)=>{
+				val = val*increase;
 				if(out.includes('!'))val = val/60;
 				out = out.replace('!','');
-				let rounded = Math.round( val * 100 ) / 100;
+				let rounded = Math.floor(Math.round( val * 100 ) / 100);
 				out = out.replace('#', rounded);
 				if(rounded === 0)out = false;
 			});
@@ -183,6 +184,10 @@ class Data {
 		this.source_url = 'https://raw.githubusercontent.com/brather1ng/RePoE/master/data/gems.json';
 		this.data = {};
 		this.gems = [];
+		this.gem_info = {};
+
+		this.decode_build();
+
 		this.download();
 	}
 
@@ -206,30 +211,150 @@ class Data {
 			});
 
 			console.log(this.gems);
-			this.gems = this.gems.sort(function(a, b) {
-				let nameA = a.name.toUpperCase(); // ignore upper and lowercase
-				let nameB = b.name.toUpperCase(); // ignore upper and lowercase
-				if (nameA < nameB) {
-					return -1;
-				}
-				if (nameA > nameB) {
-					return 1;
-				}
-				return 0;
-			});
-			this.gems.forEach((gem)=>{
-				let stats = gem.level_stats(20);
-				if(stats.length>0){
-					let out = '';
-					out+= '<b>'+gem.name+'</b>';
-					stats.forEach((txt)=>{
-						out+="<br>"+txt;
-					});
-					let cont = $("<div>").addClass('stat_block');
-					cont.html(out);
-					$("#output").append(cont);
-				}
-			})
+			this.calculate();
 		}.bind(this));
+	}
+
+	calculate(){
+		this.gems = this.gems.sort(function(a, b) {
+			let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+			let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		$("#output").empty();
+		$("#totals").html("<h2>Path of Building:</h2>Auramancer<br>Crimson Jewel<br>Limited to: 1<br>"+
+			"Implicits: 0<br>Quality: 20<br>LevelReq: 0<br>");
+
+		let total_active = 0;
+
+		this.gems.forEach((gem)=>{
+			if(!this.gem_info[gem.name]){
+				console.log('built:', gem.name);
+				this.gem_info[gem.name] = {
+					'level':gem.name === 'Clarity'?1:20,
+					'disabled':gem.name.includes('Vaal'),
+					'generosity':0
+				};
+			}
+
+			let percent_inc = parseInt($('#increase').val());
+
+			if(this.gem_info[gem.name]['generosity']>0)
+				percent_inc+= 19+parseInt(this.gem_info[gem.name]['generosity']);
+			//console.log(percent_inc, 'inc:', 1 + (percent_inc/100));
+			let stats = gem.level_stats(this.gem_info[gem.name]['level'], 1 + (percent_inc/100) );
+
+			if(stats.length>0){
+				let cont = $("<div>").addClass('stat_block');
+				let title = $('<div>').addClass('gem_title').text(gem.name);
+				let chk = $("<input>").attr("type", "checkbox");
+				let lvl = $("<input>").attr("type", "number").addClass('lvl_input').attr('title','Gem Level');
+				let gen = $("<input>").attr("type", "number").addClass('gen_input').attr('title','Generosity Level');
+				let disabled = this.gem_info[gem.name]['disabled'];
+
+				chk.prop('checked', !disabled);
+				chk.on('change', ()=>{
+					this.gem_info[gem.name]['disabled'] = !chk.prop('checked');
+					console.log(this.gem_info[gem.name]['disabled']);
+					this.calculate();
+				});
+
+				lvl.val(this.gem_info[gem.name]['level']);
+				lvl.on('change', ()=>{
+					this.gem_info[gem.name]['level'] = parseInt(lvl.val());
+					this.calculate();
+				});
+
+				gen.val(this.gem_info[gem.name]['generosity']);
+				gen.on('change', ()=>{
+					this.gem_info[gem.name]['generosity'] = parseInt(gen.val());
+					this.calculate();
+				});
+				title.append(chk);
+				title.append(lvl);
+				title.append(gen);
+				cont.append(title);
+
+				stats.forEach((txt)=>{
+					cont.append($("<div>").addClass('gem_mod').text(txt));
+					if(!disabled) {
+						$("#totals").append($('<span>').addClass('total_mod').html(txt) );
+					}
+				});
+				if(disabled)
+					cont.addClass('disabled_gem');
+				else
+					total_active++;
+				$("#output").append(cont);
+			}
+		});
+		console.log('total buffs:', total_active);
+		let necro_buffs = [
+			Math.round(3*total_active*(1 + (parseInt($('#increase').val())/100) ))+'% increased Attack and Cast Speed',
+			'30% increased Damage',
+			'+20% to all Elemental Resistances'
+		];
+		necro_buffs.forEach((buff)=>{
+			$("#totals").append($('<span>').addClass('total_mod').html(buff) );
+		});
+
+		console.log(this.gem_info);
+		window.location.hash = this.encode_build();
+	}
+
+
+	encode_build(){
+		let out = '1,';// Version 1.
+		out+= $('#increase').val()+',';
+		out+='|';
+		Object.keys(this.gem_info).forEach((idx)=> {
+			let gem = this.gem_info[idx];
+			out+=idx+',';
+			Object.keys(gem).forEach((key)=> {
+				let val = gem[key];
+				if(val === true)
+					val = 't';
+				if(val === false)
+					val = 'f';
+				out+= val+',';
+			});
+			out+='|';
+		});
+		return btoa(out);
+	}
+
+
+	decode_build(){
+		if(window.location.hash){
+			let sp = atob(window.location.hash.replace('#', '')).split('|');
+			for(let i=0; i<sp.length;i++){
+				if(sp[i].trim()==='')
+					continue;
+				let gr = sp[i].split(',');
+				if(i === 0){
+					console.log('Loading version: '+gr[0]);
+					$('#increase').val(parseInt(gr[1]));
+					continue;
+				}
+				let fields = ['level', 'disabled', 'generosity'];
+				let name = gr[0];
+				let obj = {};
+				for(let x = 0; x<fields.length;x++) {
+					let val = gr[x + 1];
+					if(val === 't')
+						val = true;
+					if(val === 'f')
+						val = false;
+					obj[fields[x]] = val;
+				}
+				this.gem_info[name] = obj;
+			}
+		}
 	}
 }
