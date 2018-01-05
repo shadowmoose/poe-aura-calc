260efbd97fc6d9e5faa2258e17260aa0c232c198
Mike <theshadowmoose@gmail.com>
2018-01-05 10:57:36 -0700

    Removed custom stat conversion in favor of in-game phrasing.
    PoB seems to handle these without issue,
    so this change will future-proof the program a bit better.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 [Go here to see it in action](https://shadowmoose.github.io/poe-aura-calc/src/)
 
-This is just a basic, very quickly-written, web-based program to let you see stats your aurabot will give to your build.
+This is just a basic, very quickly-written, web-based program to let you see stats your aurabot 
+will give to your build.
 
 It supports all aura gem levels, and each gem can be combined with a Generosity support of any level.
 
@@ -11,6 +12,7 @@ The link it generates in the nav bar can be shared, in order to share your stats
 ## Credits:
 This program was inspired by [this post](https://www.reddit.com/r/pathofexile/comments/7mfgj6/).
 
-Additionally, the program sources all in-game information from [brather1ng/RePoE](https://github.com/brather1ng/RePoE)
+Additionally, the program sources all in-game information from 
+[brather1ng/RePoE](https://github.com/brather1ng/RePoE)
 
 Go thank them for their dedication to the community!
--- src/script/data.js
+++ src/script/data.js
@@ -1,196 +1,53 @@
 class Gem{
-	constructor(name, mods, level_array){
+	constructor(name, description, mods, level_array){
 		this.name = name;
+		this.description = description;
 		this.mods = mods;
 		this.level_array = level_array;
 	}
 
 
 	/** Returns an array of formatted text lines, showing this aura's effects at the given level. */
-	level_stats(level, increase){
-		let lvl = level;//Math.max(1, Math.min(this.level_array.length, level));
+	level_stats(level, effect_increase){
+		let lvl = ""+level;//Math.max(1, Math.min(this.level_array.length, level));
 		console.log(this.name, '  level: ', lvl);
 		let stats = this.level_array[lvl]['stats'];
-		let ret = {};
-		this.mods.forEach((stat, idx)=>{
-			let val = stats[idx];
-			if(val)val = val['value'];
 
-			let name = stat['id'];
-			if(name) {
-				ret[name] = val;
-				//console.log(idx, name, val);
+		let final_vals = {};
+
+		for(let i=0; i < stats.length; i++){
+			let mod_text = this.mods[i]['text'];
+			let mod_values = stats[i]?stats[i]['values']:null;
+
+			if(mod_text.includes('radius') || mod_text.includes('duration') || !mod_values)continue;
+
+			mod_text = mod_text.replace(/{.?}/g, '#');
+			mod_text = mod_text.replace(/You and nearby allies .+? /g, '').trim();
+			if(mod_text.includes('per second'))
+				mod_text = 'Regenerate '+mod_text;
+			if(!final_vals[mod_text])
+				final_vals[mod_text] = [];
+
+			for(let idx = 0; idx<mod_values.length; idx++) {
+				let rounded = (Math.floor( mod_values[idx] * effect_increase * 100 ) / 100);
+				if(!mod_text.includes('Regenerate'))rounded =  Math.floor(rounded);
+				final_vals[mod_text].push(rounded);
 			}
-		});
-		let groups = Gem.group_stats(ret);
-		//console.log(groups);
-
-		let final = Gem.convert_to_pob(groups, increase);
-		console.log(final);
-		return final;
-	}
-
-	/** Bundles similar ("min"/"max") stats together as groups. */
-	static group_stats(stats){
-		let groups = {};
-		Object.keys(stats).forEach((key)=> {
-			let st = key.replace('minimum', '?').replace('maximum', '?');
-			if(!groups[st])
-				groups[st] = [];
-			groups[st].push(stats[key]);
-		});
-		return groups;
-	}
-
-	/** Convert internal Names into formatted strings. **/
-	static convert_internal_name(str){
-		if(Gem.match(str, 'active', 'skill', 'radius'))
-			return false;
-		if(Gem.match(str, 'deal', 'no', 'damage'))
-			return false;
-		if(Gem.match(str, 'duration'))
-			return false;
-
-		if(Gem.match(str, Gem.spell_or_attack(str), 'added', 'damage')){
-			return 'Adds # to # '+Gem.get_type(str)+' Damage to '+Gem.spell_or_attack(str)+'s';
 		}
-
-		if(Gem.match(str, 'mana', 'regen', 'minute')){
-			return '!# Mana Regenerated per Second';
-		}
-
-		if(Gem.match(str, 'base', 'resistance')){
-			let max = Gem.match(str, '?')? 'maximum ':'';
-			return '+#% to '+max+Gem.get_type(str)+' Resistance';
-		}
-
-		if(Gem.match(str, 'determination')){
-			return '#% more Armour';
-		}
-
-		if(Gem.match(str, 'energy', 'shield', 'base')){
-			return '+# to maximum Energy Shield';
-		}
-
-		if(Gem.match(str, 'energy', 'shield', 'recharge', 'rate')){
-			return false;
-		}
-
-		if(Gem.match(str, 'evasion', 'rating')){
-			return '+# to Evasion Rating';
-		}
-
-		if(Gem.match(str, "speed", 'attack')){
-			return "#% increased Attack Speed";
-		}
-
-		if(Gem.match(str, "speed", 'cast')){
-			return "#% increased Cast Speed";
-		}
-
-		if(Gem.match(str, "movement", 'velocity')){
-			return "#% increased Movement Speed";
-		}
-
-		if(Gem.match(str, "add", "as", "cold")){
-			return "#% of Physical Damage added as Cold Damage";
-		}
-
-		if(Gem.match(str, "resist", "all", "elements")){
-			return "+#% to all Elemental Resistances";
-		}
-
-		if(Gem.match(str, 'not', 'delayed', 'damage')){
-			return false;
-		}
-
-		if(Gem.match(str, 'no', 'mana', 'cost')){
-			return false;
-		}
-
-		if(Gem.match(str, 'chance', 'to', 'dodge')){
-			let spells = Gem.match(str, 'spells');
-			return '#% chance to Dodge '+(spells?'Spell Damage':'Attacks');
-		}
-
-		if(Gem.match(str, 'life', 'regeneration', 'minute')){
-			return '!#% of Life Regenerated per Second';
-		}
-
-		if(Gem.match(str, 'spell', 'lightning', 'damage', 'final')){
-			return '#% more Lightning Damage with Spells';
-		}
-
-		console.log('Unknown internal stat name:', str);
-		return str+' [#]';
-	}
-
-
-	/** Convert this Aura Gem into a Path of Building-friendly string. */
-	static convert_to_pob(grouped_stats, increase){
-		let final = {};
-		Object.keys(grouped_stats).forEach((key)=> {
-			let val_array = grouped_stats[key];
-			let out_name = this.convert_internal_name(key);
-			let out_vals = [];
-			if(!out_name)return;
-			val_array.forEach((val)=>{
-				val = val*increase;
-				if(out_name.includes('!'))val = val/60;
-				out_name = out_name.replace('!','');
-				let rounded = (Math.round( val * 100 ) / 100);
-				if(!out_name.includes('second'))rounded =  Math.floor(rounded);
-				//out_name = out_name.replace('#', rounded);
-				out_vals.push(rounded);
-				if(rounded === 0)out_name = false;
-			});
-			if(out_name)
-				final[out_name] = out_vals;
-		});
-		return final;
-	}
-
-	/** Check if the given string contains the following substrings. */
-	static match(){
-		let args = arguments;
-		let str = args[0].toLowerCase();
-		for(let i=0; i<args.length;i++){
-			if(!str.includes(args[i].toLowerCase()))
-				return false;
-		}
-		return true;
-	}
-
-	/** Get the type of damage from the given str, if any. **/
-	static get_type(str){
-		let types = ['Fire', 'Lightning', 'Cold', 'Physical', 'Chaos'];
-		let ret = 'Unknown';
-		types.forEach((ty)=>{
-			if(str.toLowerCase().includes(ty.toLowerCase()))ret = ty;
-		});
-		return ret;
-	}
-
-
-	/** Determine if this string is for a spell or an attack, and return the name. **/
-	static spell_or_attack(str){
-		if(str.toLowerCase().includes('spell'))
-			return 'Spell';
-		if(str.toLowerCase().includes('attack'))
-			return 'Attack';
-		return 'Unknown';
+		return final_vals;
 	}
 }
 
+
+
 class Data {
 	constructor() {
-		this.source_url = 'https://raw.githubusercontent.com/brather1ng/RePoE/master/data/gems.json';
+		this.source_url = 'https://raw.githubusercontent.com/brather1ng/RePoE/master/data/gem_tooltips.min.json';
 		this.data = {};
 		this.gems = [];
 		this.gem_info = {};
 
 		this.decode_build();
-
 		this.download();
 	}
 
@@ -201,16 +58,16 @@ class Data {
 
 			Object.keys(this.data).forEach((key)=> {
 				let ele = this.data[key];
-				let base_item = ele['base_item'];
-				if (!ele['tags'] ||
-					!ele['tags'].includes('aura') ||
-					ele['tags'].includes('support') ||
-					ele['tags'].includes('totem'))
+				let base_item = ele['static'];
+				if (!base_item['properties'] ||!base_item['properties'][0]||
+					!base_item['properties'][0].includes('Aura') ||
+					base_item['properties'][0].includes('Support') ||
+					base_item['properties'][0].includes('Totem'))
 					return;
-				if(base_item['release_state'] !== 'released')
-					return;
-				this.gems.push(new Gem(base_item['display_name'], ele['static']['stats'], ele['per_level']));
-				console.log(base_item['display_name']);
+				this.gems.push(
+					new Gem(base_item['name'], base_item['description'][0], base_item['stats'], ele['per_level'])
+				);
+				console.log(base_item['name']);
 			});
 
 			console.log(this.gems);
@@ -221,8 +78,8 @@ class Data {
 
 	calculate(){
 		this.gems = this.gems.sort(function(a, b) {
-			let nameA = a.name.toUpperCase(); // ignore upper and lowercase
-			let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+			let nameA = a.name.toUpperCase();
+			let nameB = b.name.toUpperCase();
 			if (nameA < nameB) {
 				return -1;
 			}
@@ -252,12 +109,12 @@ class Data {
 
 			if(this.gem_info[gem.name]['generosity']>0)
 				percent_inc+= 19+parseInt(this.gem_info[gem.name]['generosity']);
-			//console.log(percent_inc, 'inc:', 1 + (percent_inc/100));
+
 			let stats = gem.level_stats(this.gem_info[gem.name]['level'], 1 + (percent_inc/100) );
 
 			if(Object.keys(stats).length>0){
 				let cont = $("<div>").addClass('stat_block');
-				let title = $('<div>').addClass('gem_title').text(gem.name);
+				let title = $('<div>').addClass('gem_title').text(gem.name).attr('title', gem.description);
 				let chk = $("<input>").attr("type", "checkbox");
 				let lvl = $("<input>").attr("type", "number").addClass('lvl_input').attr('title','Gem Level');
 				let gen = $("<input>").attr("type", "number").addClass('gen_input').attr('title','Generosity Level');
@@ -296,13 +153,13 @@ class Data {
 					});
 					cont.append($("<div>").addClass('gem_mod').text(txt));
 				});
-				if(disabled)
+				if(disabled) {
 					cont.addClass('disabled_gem');
-				else {
+				} else {
 					total_active++;
 					total_speed_buff+= 3*( 1 + (percent_inc/100) );
 					Object.keys(stats).forEach((stat)=> {
-						// if not disabled, append this gem's normalized mod objects to the total grouped mods.
+						// if not disabled, add this gem's normalized mod objects to the total grouped mods.
 						if(!grouped_stats[stat])
 							grouped_stats[stat] = stats[stat];
 						else
@@ -321,9 +178,7 @@ class Data {
 			'#% increased Attack Speed':[Math.floor(total_speed_buff)],
 			'#% increased Cast Speed':[Math.floor(total_speed_buff)],
 			'#% increased Damage':[30],
-			'+#% to Fire Resistance':[20],
-			'+#% to Cold Resistance':[20],
-			'+#% to Lightning Resistance':[20]
+			'+#% to all Elemental Resistances':[20],
 		};
 		Object.keys(necro_buffs).forEach((stat)=> {
 			if(!grouped_stats[stat])
